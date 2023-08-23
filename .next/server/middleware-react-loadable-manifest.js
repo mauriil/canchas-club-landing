aee6e7b33b7f587f1412227130324c43cc7b412f
Mauri Lahitte <mauriciolahitte@Mauricios-MacBook-Air.local>
2023-08-23 15:13:17 -0300

    updating gitignore

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"../components/Sections/ParallaxVideo.js -> react-modal-video\":{\"id\":\"../components/Sections/ParallaxVideo.js -> react-modal-video\",\"files\":[\"static/chunks/node_modules_react-modal-video_lib_index_js.js\"]}}"
