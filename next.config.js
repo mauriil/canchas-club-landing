7f859b2c060b5eeb2e0e0e13b600836a87372450
Mauri Lahitte <mauriciolahitte@Mauricios-MacBook-Air.local>
2023-11-01 17:04:52 -0300

    test config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,10 @@
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
-    distDir: '_static',
+    // distDir: '_static',
     images: {
       unoptimized: true
     },
-    output: 'export',
   }
 
 module.exports = nextConfig
