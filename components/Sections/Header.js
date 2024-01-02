40c4834979cbdad100304e76de7df5e5da505b76
Mauri Lahitte <mauricio.mm94@gmail.com>
2024-01-02 13:27:53 -0300

    comment "screenshot" section in header

--- components/Sections/Header.js
+++ components/Sections/Header.js
@@ -22,12 +22,12 @@ const Header = (props) => {
                 <div id="mainNav" className={isMobileMenu ? 'main-menu-area animated mobile-menu-open' : 'main-menu-area animated'}>
                     <Container>
                         <Row className="align-items-center">
-                            
+
                             <Col className="col-12 col-lg-2 d-flex justify-content-between align-items-center">
 
                                 {/* <!-- Logo --> */}
                                 <div className="logo">
-                                    
+
                                     <Link href="/" className="navbar-brand navbar-brand1">
                                         <img src="/images/CanchasClub_SubLogo.png" srcSet={`/images/CanchasClub_SubLogo.png 2x`} alt="CanchasClub" />
                                     </Link>
@@ -35,11 +35,11 @@ const Header = (props) => {
                                     <Link href="/" className="navbar-brand navbar-brand2">
                                         <img src="/images/CanchasClub_SubLogo.png" srcSet={`/images/CanchasClub_SubLogo.png 2x`} alt="CanchasClub" />
                                     </Link>
-                                
+
                                 </div>
 
                                 {/* <!-- Burger menu --> */}
-                                <div className="menu-bar d-lg-none" role="button" tabIndex={0} onKeyPress={() => {}} onClick={handleMobileMenu}>
+                                <div className="menu-bar d-lg-none" role="button" tabIndex={0} onKeyPress={() => { }} onClick={handleMobileMenu}>
                                     <span></span>
                                     <span></span>
                                     <span></span>
@@ -47,56 +47,56 @@ const Header = (props) => {
 
                             </Col>
 
-                            <div className="op-mobile-menu col-lg-10 p-0 d-lg-flex align-items-center justify-content-end" role="button" tabIndex={0} onKeyPress={() => {}} onClick={handleMobileMenu}>
-                            
+                            <div className="op-mobile-menu col-lg-10 p-0 d-lg-flex align-items-center justify-content-end" role="button" tabIndex={0} onKeyPress={() => { }} onClick={handleMobileMenu}>
+
                                 {/* <!-- Mobile menu --> */}
                                 <div className="m-menu-header d-flex justify-content-between align-items-center d-lg-none">
-                                    
+
                                     {/* <!-- Logo --> */}
                                     <a href="#" className="logo">
                                         <img src="images/logo.png" srcSet={`/images/CanchasClub_Logo_FondoTransparente-menu.png 2x`} alt="CanchasClub" />
                                     </a>
-                                    
+
                                     {/* <!-- Close button --> */}
-                                    <span className="close-button" role="button" tabIndex={0} onKeyPress={() => {}} onClick={handleMobileMenu}></span>
-                                    
+                                    <span className="close-button" role="button" tabIndex={0} onKeyPress={() => { }} onClick={handleMobileMenu}></span>
+
                                 </div>
-                                
+
                                 {/* <!-- Items --> */}
                                 <ul className="nav-menu d-lg-flex flex-wrap list-unstyled justify-content-center">
-                                    
+
                                     <li className="nav-item">
-                                        <Link href="/#top-page" className={props.nav !== undefined && props.nav==="home" ? 'nav-link js-scroll-trigger active' : 'nav-link js-scroll-trigger'}>
+                                        <Link href="/#top-page" className={props.nav !== undefined && props.nav === "home" ? 'nav-link js-scroll-trigger active' : 'nav-link js-scroll-trigger'}>
                                             <span>Inicio</span>
                                         </Link>
                                     </li>
-                                    
+
                                     <li className="nav-item">
                                         <Link href="/#features" className="nav-link js-scroll-trigger">
                                             <span>Características</span>
                                         </Link>
                                     </li>
-                                    
-                                    <li className="nav-item">
+
+                                    {/* <li className="nav-item">
                                         <Link href="/#screenshots" className="nav-link js-scroll-trigger">
                                             <span>Screenshots</span>
                                         </Link>
-                                    </li>
-                                    
+                                    </li> */}
+
                                     <li className="nav-item">
                                         <Link href="/#support" className="nav-link js-scroll-trigger">
                                             <span>Soporte</span>
                                         </Link>
                                     </li>
-                                    
+
                                     <li className="nav-item">
-                                        <Link href="/#pricing" className="nav-link js-scroll-trigger"> 
+                                        <Link href="/#pricing" className="nav-link js-scroll-trigger">
                                             <span>Planes</span>
                                         </Link>
                                     </li>
 
                                 </ul>
-                                
+
                             </div>
 
                         </Row>
